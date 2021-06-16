cd7ec041e253957bf0e0f5281ada0a7915a9581f
tanku3080 <57029371+tanku3080@users.noreply.github.com>
2021-06-16 22:47:45 +0900

    追加の環境絵とopeningムービーシーンの実装完了

---
+++ TextAdventure/Assets/script/OPScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OPScript : MonoBehaviour
+{
+    [SerializeField] VideoPlayer videoOBJ = null;
+    [SerializeField] GameObject warningObj = null;
+    [SerializeField] CanvasGroup group = null;
+    [SerializeField] GameObject skipObj = null;
+    [SerializeField] GameObject backGround = null;
+    [SerializeField] AudioClip pushSFX = null;
+    [SerializeField] AudioSource source = null;
+    void Start()
+    {
+        skipObj.SetActive(false);
+        backGround.SetActive(true);
+        StartCoroutine(WaeninngObjFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (videoOBJ.isPlaying)
+        {
+            if (Input.GetKeyUp(KeyCode.Return))
+            {
+                videoOBJ.Pause();
+                skipObj.SetActive(true);
+            }
+        }
+    }
+
+    IEnumerator WaeninngObjFade()
+    {
+        while (true)
+        {
+            yield return null;
+            if (group.alpha >= 1)
+            {
+                yield return new WaitForSeconds(4);
+                while (true)
+                {
+                    yield return null;
+                    if (group.alpha <= 0) break;
+                    else group.alpha -= 0.001f;
+                }
+                break;
+            }
+            else group.alpha += 0.001f;
+        }
+        OPStart();
+        yield return 0;
+    }
+    public void YesObj()
+    {
+        source.PlayOneShot(pushSFX);
+        SceneManager.LoadScene("Title");
+    }
+    public void NoObj()
+    {
+        skipObj.SetActive(false);
+        videoOBJ.Play();
+    }
+
+    private void OPStart()
+    {
+        videoOBJ.Play();
+        backGround.SetActive(false);
+    }
+}
